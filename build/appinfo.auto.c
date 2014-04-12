ba5c3721f83ccdf857791a996132f0c3c39a1377
JamesFowler42 <keith@difficult.me.uk>
2014-04-13 00:13:46 +0100

    1.0 Pre-release
    Initial version

---
+++ build/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_app_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleAppInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { APP_INFO_CURRENT_STRUCT_VERSION_MAJOR, APP_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { APP_INFO_CURRENT_SDK_VERSION_MAJOR, APP_INFO_CURRENT_SDK_VERSION_MINOR },
+  .app_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "Badger Weather",
+  .company = "Roth/Fowler",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = APP_INFO_WATCH_FACE,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x7F, 0xF0, 0x1B, 0xBB, 0x07, 0x26, 0x4A, 0xB0, 0xBB, 0x2E, 0x2B, 0xAB, 0x40, 0x42, 0x43, 0xC8 },
+  .virtual_size = 0xb6b6
+};
---
+++ src/badgerweather.c
@@ -0,0 +1,197 @@
+/*
+ * Badger Weather
+ *
+ * Copyright (c) 2013 James Fowler/Indigo Roth
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+
+#include "pebble.h"
+#include "common.h"
+
+static Window *window;
+
+static TextLayer *temperature_layer;
+static BitmapLayer *icon_layer;
+static GBitmap *icon_bitmap = NULL;
+static TextLayer *text_time_layer;
+static char time_text[6];
+
+static AppSync sync;
+static uint8_t sync_buffer[64];
+
+IconEntry icon_lookup[] = {
+                           { .icon="???", .resource_id=RESOURCE_ID_CLUELESS, .tm_x=0, .tm_y=112, .tm_w=143, .tm_h=168-112, .tp_x=0, .tp_y=94, .tp_w=143, .tp_h=168-94},
+                           { .icon="01d", .resource_id=RESOURCE_ID_DAY_SUN, .tm_x=0, .tm_y=112, .tm_w=143, .tm_h=168-112, .tp_x=0, .tp_y=94, .tp_w=143, .tp_h=168-94},
+                           { .icon="01n", .resource_id=RESOURCE_ID_NIGHT_CLEAR, .tm_x=0, .tm_y=112, .tm_w=143, .tm_h=168-112, .tp_x=0, .tp_y=94, .tp_w=143, .tp_h=168-94},
+                           { .icon="02d", .resource_id=RESOURCE_ID_DAY_SUN_CLOUD, .tm_x=0, .tm_y=112, .tm_w=143, .tm_h=168-112, .tp_x=0, .tp_y=94, .tp_w=143, .tp_h=168-94},
+                           { .icon="02n", .resource_id=RESOURCE_ID_NIGHT_CLEAR_CLOUD, .tm_x=0, .tm_y=112, .tm_w=143, .tm_h=168-112, .tp_x=0, .tp_y=94, .tp_w=143, .tp_h=168-94},
+                           { .icon="03d", .resource_id=RESOURCE_ID_CLOUD, .tm_x=0, .tm_y=112, .tm_w=143, .tm_h=168-112, .tp_x=0, .tp_y=94, .tp_w=143, .tp_h=168-94},
+                           { .icon="03n", .resource_id=RESOURCE_ID_CLOUD, .tm_x=0, .tm_y=112, .tm_w=143, .tm_h=168-112, .tp_x=0, .tp_y=94, .tp_w=143, .tp_h=168-94},
+                           { .icon="04d", .resource_id=RESOURCE_ID_BROKEN_CLOUD, .tm_x=0, .tm_y=112, .tm_w=143, .tm_h=168-112, .tp_x=0, .tp_y=94, .tp_w=143, .tp_h=168-94},
+                           { .icon="04n", .resource_id=RESOURCE_ID_BROKEN_CLOUD, .tm_x=0, .tm_y=112, .tm_w=143, .tm_h=168-112, .tp_x=0, .tp_y=94, .tp_w=143, .tp_h=168-94},
+                           { .icon="09d", .resource_id=RESOURCE_ID_SHOWERS, .tm_x=0, .tm_y=112, .tm_w=143, .tm_h=168-112, .tp_x=0, .tp_y=94, .tp_w=143, .tp_h=168-94},
+                           { .icon="09n", .resource_id=RESOURCE_ID_SHOWERS, .tm_x=0, .tm_y=112, .tm_w=143, .tm_h=168-112, .tp_x=0, .tp_y=94, .tp_w=143, .tp_h=168-94},
+                           { .icon="10d", .resource_id=RESOURCE_ID_DAY_RAIN, .tm_x=0, .tm_y=112, .tm_w=143, .tm_h=168-112, .tp_x=0, .tp_y=94, .tp_w=143, .tp_h=168-94},
+                           { .icon="10n", .resource_id=RESOURCE_ID_NIGHT_RAIN, .tm_x=0, .tm_y=112, .tm_w=143, .tm_h=168-112, .tp_x=0, .tp_y=94, .tp_w=143, .tp_h=168-94},
+                           { .icon="11d", .resource_id=RESOURCE_ID_THUNDERSTORM, .tm_x=0, .tm_y=112, .tm_w=143, .tm_h=168-112, .tp_x=0, .tp_y=94, .tp_w=143, .tp_h=168-94},
+                           { .icon="11n", .resource_id=RESOURCE_ID_THUNDERSTORM, .tm_x=0, .tm_y=112, .tm_w=143, .tm_h=168-112, .tp_x=0, .tp_y=94, .tp_w=143, .tp_h=168-94},
+                           { .icon="13d", .resource_id=RESOURCE_ID_SNOW, .tm_x=0, .tm_y=112, .tm_w=143, .tm_h=168-112, .tp_x=0, .tp_y=94, .tp_w=143, .tp_h=168-94},
+                           { .icon="13n", .resource_id=RESOURCE_ID_SNOW, .tm_x=0, .tm_y=112, .tm_w=143, .tm_h=168-112, .tp_x=0, .tp_y=94, .tp_w=143, .tp_h=168-94},
+                           { .icon="50d", .resource_id=RESOURCE_ID_FOG, .tm_x=0, .tm_y=112, .tm_w=143, .tm_h=168-112, .tp_x=0, .tp_y=94, .tp_w=143, .tp_h=168-94},
+                           { .icon="50n", .resource_id=RESOURCE_ID_FOG, .tm_x=0, .tm_y=112, .tm_w=143, .tm_h=168-112, .tp_x=0, .tp_y=94, .tp_w=143, .tp_h=168-94}
+};
+
+static void sync_error_callback(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Sync Error: %d", app_message_error);
+}
+
+static IconEntry* findInfo(const char *icon) {
+  for (uint16_t i=0; i < ARRAY_LENGTH(icon_lookup); i++) {
+    if (strncmp(icon_lookup[i].icon, icon, sizeof(icon_lookup[i].icon)) == 0) {
+      return &icon_lookup[i];
+    }
+  }
+  return &icon_lookup[0];
+}
+
+static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tuple, const Tuple* old_tuple, void* context) {
+  switch (key) {
+    case WEATHER_ICON_KEY:
+      {
+        IconEntry *new_info = findInfo(new_tuple->value->cstring);
+
+      if (icon_bitmap) {
+        gbitmap_destroy(icon_bitmap);
+      }
+      icon_bitmap = gbitmap_create_with_resource(new_info->resource_id);
+      bitmap_layer_set_bitmap(icon_layer, icon_bitmap);
+      layer_set_frame(text_layer_get_layer(text_time_layer), GRect(new_info->tm_x, new_info->tm_y, new_info->tm_w, new_info->tm_h));
+      layer_set_frame(text_layer_get_layer(temperature_layer), GRect(new_info->tp_x, new_info->tp_y, new_info->tp_w, new_info->tp_h));
+      }
+      break;
+
+    case WEATHER_TEMPERATURE_KEY:
+      // App Sync keeps new_tuple in sync_buffer, so we may use it directly
+      text_layer_set_text(temperature_layer, new_tuple->value->cstring);
+      break;
+
+  }
+}
+
+static void send_cmd(void) {
+  Tuplet value = TupletInteger(1, 1);
+
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+
+  if (iter == NULL) {
+    return;
+  }
+
+  dict_write_tuplet(iter, &value);
+  dict_write_end(iter);
+
+  app_message_outbox_send();
+}
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+
+  icon_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+  layer_add_child(window_layer, bitmap_layer_get_layer(icon_layer));
+
+  temperature_layer = text_layer_create(GRect(0, 95, 144, 68));
+  text_layer_set_text_color(temperature_layer, GColorWhite);
+  text_layer_set_background_color(temperature_layer, GColorClear);
+  text_layer_set_font(temperature_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
+  text_layer_set_text_alignment(temperature_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(temperature_layer));
+
+  text_time_layer = text_layer_create(GRect(0, 112, 143, 168-112));
+  text_layer_set_text_color(text_time_layer, GColorWhite);
+  text_layer_set_background_color(text_time_layer, GColorClear);
+  text_layer_set_font(text_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+  text_layer_set_text_alignment(text_time_layer, GTextAlignmentCenter);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(text_time_layer));
+
+  Tuplet initial_values[] = {
+    TupletCString(WEATHER_ICON_KEY, "01d"),
+    TupletCString(WEATHER_TEMPERATURE_KEY, "1234\u00B0C")
+  };
+
+  app_sync_init(&sync, sync_buffer, sizeof(sync_buffer), initial_values, ARRAY_LENGTH(initial_values),
+      sync_tuple_changed_callback, sync_error_callback, NULL);
+
+  send_cmd();
+}
+
+static void window_unload(Window *window) {
+  app_sync_deinit(&sync);
+
+  if (icon_bitmap) {
+    gbitmap_destroy(icon_bitmap);
+  }
+
+  text_layer_destroy(text_time_layer);
+  text_layer_destroy(temperature_layer);
+  bitmap_layer_destroy(icon_layer);
+}
+
+/*
+ * Clock tick
+ */
+static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
+
+  clock_copy_time_string(time_text, sizeof(time_text));
+  if (time_text[4] == ' ')
+    time_text[4] = '\0';
+  text_layer_set_text(text_time_layer, time_text);
+
+}
+
+static void init(void) {
+  window = window_create();
+  window_set_background_color(window, GColorBlack);
+  window_set_fullscreen(window, true);
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload
+  });
+
+  const int inbound_size = 64;
+  const int outbound_size = 64;
+  app_message_open(inbound_size, outbound_size);
+
+  window_stack_push(window, true);
+
+  tick_timer_service_subscribe(MINUTE_UNIT, &handle_minute_tick);
+}
+
+static void deinit(void) {
+  tick_timer_service_unsubscribe();
+  window_destroy(window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
